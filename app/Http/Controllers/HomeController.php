a81905f0bac7d9b730f187daa25bbe2034eb6789
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-08-26 12:55:19 +0200

    printed Train db data in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $departingTrains = Train::whereDate('orario_partenza', '>=', $currentDate)->get();
+
+        return view('home', ['departingTrains' => $departingTrains]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Home')
 
 @section('main')
-
+    <h1>Departing Trains</h1>
+    <ul>
+        @foreach ($departingTrains as $train)
+            <li>
+                {{ $train->azienda }} - {{ $train->codice_treno }} | Partenza: {{ $train->orario_partenza }} | Arrivo:
+                {{ $train->orario_arrivo }}
+            </li>
+        @endforeach
+    </ul>
 @endsection
